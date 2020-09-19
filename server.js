487dc88602ffad452fe3c62238b9d8cec8c837d1
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-09-19 20:21:02 +0700

    add mongo client

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.get('/', function(req, res){
-	res.send('this is practice for RESTful API');
-});
+mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true})
+		.then(()=>{
+			app.get('/', function(req, res){
+				res.send('this is practice for RESTful API');
 
-app.listen(3000, ()=>{
-	console.log('server listening')
-});
+			});
+
+			app.listen(3000, ()=>{
+				console.log('server listening')
+
+			});			
+		})
+
+
+

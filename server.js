331186fb8ca2404135cf3e4401b037f2f33fe830
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-09-21 12:06:45 +0700

    arrange folder structure

--- server.js
+++ server.js
@@ -2,18 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true})
-		.then(()=>{
-			app.get('/', function(req, res){
-				res.send('this is practice for RESTful API');
+const { dbUrl } = require('./config/db')
+const postRoute = require('./routes/post')
 
-			});
+mongoose.connect(dbUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+.then(() => {
+	console.log('dbconnected',)
+})
+.catch((err)=>{
+	console.log(err)
+})
 
-			app.listen(3000, ()=>{
-				console.log('server listening')
-
-			});			
-		})
-
+app.use('/post', postRoute)
+app.use('/', (req,res)=>{
+	res.send('hello from home')
+})
 
 
+app.listen(5000,() =>{
+	console.log('server listening')
+}) 

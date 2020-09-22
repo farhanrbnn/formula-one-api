11ab25e5765e70611e1053a563bef403dacde99b
Farhan Rabbaanii <farhanrbnn@gmail.com>
2020-09-22 11:30:44 +0700

    make home component

---
+++ client/src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h3 align="center">Formula One API</h3>
+    <h4>Select Team</h4>
+    <div id="dropdown">
+      <b-form-select v-model="selected" :options="options"></b-form-select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#dropdown {
+  width: 500px;
+
+}
+</style>
